1ea5482e13ee5106aeec0e4491fef16149f7c2fb
Jeme2552 <medeirosjjv@gmail.com>
2020-03-27 13:29:34 -0300

    front-end aplicação finalizada

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -15,7 +15,7 @@ export default function Logon(){
         try{
           const response = await api.post('sessions', {id}) ; 
           localStorage.setItem('ongId', id);
-          localStorage.setItem('PongName', response.data.name);
+          localStorage.setItem('ongName', response.data.name);
           history.push('/profile');  
 
         }catch (err){
